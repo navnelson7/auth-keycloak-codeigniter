5d66a12817e07d1b1495851aa1a5ccd37cb5eba7
navnelson7 <navnelson7@gmail.com>
2022-11-10 15:34:14 -0600

    generate parameters and request loginUrl

--- app/Controllers/SigninController.php
+++ app/Controllers/SigninController.php
@@ -1,5 +1,7 @@
 <?php 
 namespace App\Controllers;  
+use Ataccama\Adapters\Keycloak;
+use Ataccama\Utils\KeycloakAPI;
 use CodeIgniter\Controller;
 use App\Models\UserModel;
 use App\Libraries\Authkeycloak;
@@ -14,8 +16,16 @@ class SigninController extends Controller
         echo view('signin');
     } 
 
-    public function loginAuth()
-    {
+    public function loginAuth(){
+        $parameters = array(
+            "host"=>"http://localhost:8080/",
+            "realmId"=>"diaspora",
+            "clientId"=>"diaspora-sv",
+        );
+        $keycloak = new Keycloak($parameters);
+        $myauth = new Authkeycloak($keycloak);
+        $loginUrl = $myauth->getLoginUrl();
+        $myauth->authorize($_GET['code']);
         echo view('welcome_message');
     }
 }
